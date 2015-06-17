7cb98e9234e82077ccdf6dad1cc3817109bac6f8
June Wu <junewu.canton@gmail.com>
2015-06-17 17:30:35 +0800

    use requestAnimationFrame instead of setInterval

--- timeline.js
+++ timeline.js
@@ -5,6 +5,33 @@ define([
   Helper,
   EV
 ){
+  (function() {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+      window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||    // Webkit中此取消方法的名字变了
+      window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame) {
+      window.requestAnimationFrame = function(callback, element) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
+        var id = window.setTimeout(function() {
+          callback(currTime + timeToCall);
+        }, timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+      };
+    }
+    if (!window.cancelAnimationFrame) {
+      window.cancelAnimationFrame = function(id) {
+        clearTimeout(id);
+      };
+    }
+  }());
+
   var TimeLine= function(){
     var _this=this;
     EV.call(_this);
@@ -19,21 +46,26 @@ define([
     var _this=this;
     _this.startTime=(new Date()).getTime()-_this.timeOffset;
     _this.status='running';
-    _this.runningTimer=setInterval(function(){
+    // _this.runningTimer=setInterval(function(){
+    //   _this.updateTime();
+    // }, 1000 / 24);
+    _this.runningTimer=window.requestAnimationFrame(function(){
       _this.updateTime();
-    }, 1000 / 24);
+    });
   };
 
   TimeLine.prototype.stop=function(){
     var _this=this;
-    clearInterval(_this.runningTimer);
+    window.cancelAnimationFrame(_this.runningTimer);
+    // clearInterval(_this.runningTimer);
     _this.status='stop';
     _this.timeOffset=0;
   };
 
   TimeLine.prototype.pause=function(){
     var _this=this;
-    clearInterval(_this.runningTimer);
+    window.cancelAnimationFrame(_this.runningTimer);
+    // clearInterval(_this.runningTimer);
     _this.status='paused';
   };
 
@@ -41,6 +73,9 @@ define([
     var _this=this;
     _this.currentTime=(new Date()).getTime();
     _this.timeOffset=_this.currentTime-_this.startTime;
+    _this.runningTimer=window.requestAnimationFrame(function(){
+      _this.updateTime();
+    });
     _this.trigger('timeUpdate', _this.timeOffset);
   };
 
